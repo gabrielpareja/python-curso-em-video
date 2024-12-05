66858cb51743d414f7282df916d3a03a9bd6b733
gabrielpareja <gabriels.pareja@gmail.com>
2024-12-05 12:08:15 -0300

    Alterando a forma de formatação do print

--- Aula 06b.py
+++ Aula 06b.py
@@ -3,4 +3,4 @@
 n1 = int(input('Digite um número'))
 n2 = int(input('Digite o segunto número'))
 s = (n1+n2)
-print ('A soma de {} com {} é {}' .format(n1,n2,s))
+print (f'A soma de {n1} com {n2} é {s}' )
