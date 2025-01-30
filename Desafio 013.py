9f08ec5bd264f12f1ddd4425988ecc3572a1e167
Gabriel Afonso <gabriels.pareja@gmail.com>
2025-01-30 10:21:50 -0300

    Alteração desafio

--- Desafio 013.py
+++ Desafio 013.py
@@ -1,1 +1,4 @@
 #Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+s = float(input('Qual o salário que você deseja receber aumento? \n'))
+a = s * 1.15
+print(f'O salário de {s} com 15% de aumento é {a}')
